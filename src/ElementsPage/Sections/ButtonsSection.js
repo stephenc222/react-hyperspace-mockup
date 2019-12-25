b01a331ec4837a196ce192f0de97b931c2bf5b97
Stephen Collins <stephenc211@gmail.com>
2019-12-24 23:20:36 -0600

    implements Buttons section up to 'Fit' buttons

--- src/ElementsPage/Sections/ButtonsSection.js
+++ src/ElementsPage/Sections/ButtonsSection.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Section from './Section'
+import Button from '../../components/Button'
 
 const ButtonsSection = () => {
   return (
     <Section header='Buttons'>
-      TODO
+      <div className='list-section-column'>
+        <div style={{ display: 'flex', paddingBottom: 30 }}>
+          <Button inverse label='Primary' />
+          <span style={{ padding: 10 }} />
+          <Button label='Default' />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <Button size='large' label='large' />
+          <span style={{ padding: 10 }} />
+          <Button label='default' />
+          <span style={{ padding: 10 }} />
+          <Button size='small' label='small' />
+        </div>
+      </div>
     </Section>
   )
 }
