b7ce71919e7a48f1cd185ab95f26bc469782c541
Stephen Collins <stephenc211@gmail.com>
2019-12-24 23:08:06 -0600

    implements Table Section

--- src/ElementsPage/Sections/TableSection.js
+++ src/ElementsPage/Sections/TableSection.js
@@ -4,7 +4,98 @@ import Section from './Section'
 const TableSection = () => {
   return (
     <Section header='Table'>
-      TODO
+      <div className='table-section-content' >
+        <h3>Default</h3>
+        <div className="table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Item One</td>
+                <td>Ante turpis integer aliquet porttitor.</td>
+                <td>29.99</td>
+              </tr>
+              <tr>
+                <td>Item Two</td>
+                <td>Vis ac commodo adipiscing arcu aliquet.</td>
+                <td>19.99</td>
+              </tr>
+              <tr>
+                <td>Item Three</td>
+                <td> Morbi faucibus arcu accumsan lorem.</td>
+                <td>29.99</td>
+              </tr>
+              <tr>
+                <td>Item Four</td>
+                <td>Vitae integer tempus condimentum.</td>
+                <td>19.99</td>
+              </tr>
+              <tr>
+                <td>Item Five</td>
+                <td>Ante turpis integer aliquet porttitor.</td>
+                <td>29.99</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="2"></td>
+                <td>100.00</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <h3>Alternate</h3>
+        <div className="table-wrapper">
+          <table className="alt">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Item One</td>
+                <td>Ante turpis integer aliquet porttitor.</td>
+                <td>29.99</td>
+              </tr>
+              <tr>
+                <td>Item Two</td>
+                <td>Vis ac commodo adipiscing arcu aliquet.</td>
+                <td>19.99</td>
+              </tr>
+              <tr>
+                <td>Item Three</td>
+                <td> Morbi faucibus arcu accumsan lorem.</td>
+                <td>29.99</td>
+              </tr>
+              <tr>
+                <td>Item Four</td>
+                <td>Vitae integer tempus condimentum.</td>
+                <td>19.99</td>
+              </tr>
+              <tr>
+                <td>Item Five</td>
+                <td>Ante turpis integer aliquet porttitor.</td>
+                <td>29.99</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="2"></td>
+                <td>100.00</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
     </Section>
   )
 }
